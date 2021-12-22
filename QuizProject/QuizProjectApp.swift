9b367cfa2d2929ea09de6bb08986715ff598cc69
Martin Dinev <martindinev@Martins-MacBook-Air.local>
2021-12-22 11:45:04 +0200

    Initial project with Firebase dependency

---
+++ QuizProject/QuizProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizProjectApp.swift
+//  QuizProject
+//
+//  Created by Martin Dinev on 22.12.21.
+//
+
+import SwiftUI
+
+@main
+struct QuizProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
